76d2477cc91681dfcdbd05d1c6ebc9cc8a409a04
fa-yoshinobu <80754325+fa-yoshinobu@users.noreply.github.com>
2026-06-14 20:27:35 +0900

    Prepare 0.8.0 release

--- toyopuc/__init__.py
+++ toyopuc/__init__.py
@@ -14,7 +14,7 @@ but the helpers above are the preferred surface for normal application code
 and generated user documentation.
 """
 
-__version__ = "0.1.8"
+__version__ = "0.8.0"
 
 from .address import (
     encode_bit_address,
